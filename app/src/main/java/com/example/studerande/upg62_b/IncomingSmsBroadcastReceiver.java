1b91ecda2e54db777dfcd93511f7bda48eaee58f
Moses Johansson <mosesjohansson@gmail.com>
2017-03-19 00:36:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/studerande/upg62_b/IncomingSmsBroadcastReceiver.java
@@ -0,0 +1,54 @@
+package com.example.studerande.upg62_b;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by Studerande on 2017-03-18.
+ */
+
+public class IncomingSmsBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+
+        if (intent != null && SMS_RECEIVED.equals(intent.getAction())) {
+            final SmsMessage smsMessage = extractSmsMessage(intent);
+            processMessage(context, smsMessage);
+
+        }
+
+    }
+
+    private SmsMessage extractSmsMessage(final Intent intent) {
+
+        final Bundle pudsBundle = intent.getExtras();
+        final Object[] pdus = (Object[]) pudsBundle.get("pdus");
+        String format = pudsBundle.getString("format");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[0], format);
+            return smsMessage;
+        }
+        else {
+            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[0]);
+            return smsMessage;
+        }
+
+
+    }
+
+    private void processMessage(final Context context, final SmsMessage smsMessage) {
+        // Do something interesting here
+        String msg = smsMessage.getMessageBody();
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/studerande/upg62_b/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.studerande.upg62_b;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_RECEIVE_SMS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getPermissionToReceiveSMS();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void getPermissionToReceiveSMS() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.RECEIVE_SMS)) {
+                    // Show our own UI to explain to the user why we need to read the contacts
+                    // before actually requesting the permission and showing the default UI
+                }
+            }
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, REQUEST_RECEIVE_SMS);
+        }
+    }
+}
